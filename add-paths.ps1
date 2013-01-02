077ae42dfa2b40d8c88b0f3a8ac6de2ee076ab21
Dzmitry Lahoda <asd.and.rizzo@gmail.com>
2013-01-02 18:21:11 +0300

    up

--- add-paths.ps1
+++ add-paths.ps1
@@ -1,5 +1,8 @@
 # creates environment variables for sofware I use in windows
-$paths ="c:\Program Files (x86)\Microsoft F#\v4.0\;"
-$Env:Path = $Env:Path +";"+$paths
+$paths ="c:\Program Files (x86)\Microsoft F#\v4.0\;c:\scripts;"
+if (!$Env:Path.contains($paths)){ 
+  [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $paths, "Machine")
+  }
+$Env:Path
 $extensions =".fsx;"
 $Env:PATHEXT = $Env:PATHEXT +";"+$extensions
