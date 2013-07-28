e6a3962c5204fb674fee304fde97f5592c904fb8
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-07-28 19:58:09 +0300

    up clean up scripts

--- netflush.bat
+++ netflush.bat
@@ -3,3 +3,10 @@
 ipconfig /flushdns
 netsh interface ip delete arpcache
 ipconfig /renew
+
+:: reset TCP/IP stack
+netsh int ip reset c:\resetlog.txt   
+:: reset winsock
+netsh winsock reset c:\winsock.txt
+:: reset IPv4 TCP/IP stack
+netsh int ipv4 reset C:\reset.log
