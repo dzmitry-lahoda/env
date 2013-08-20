4322619d7f9df803873854eb73bb0c0d164f4c96
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-08-20 09:10:32 +0300

    update shortcuts scripts

--- add-paths.ps1
+++ add-paths.ps1
@@ -1,13 +1,27 @@
 # creates environment variables for sofware programmer Dzmitry Lahoda use in Windows
-$paths ="c:\Program Files (x86)\Microsoft F#\v4.0\;c:\scripts;"
-if (!$Env:Path.contains($paths)){ 
-  [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $paths, "Machine")
+$fsharp ="c:\Program Files (x86)\Microsoft F#\v4.0\;"
+if (!$Env:Path.contains($fsharp)){ 
+  [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $fsharp+";", "Machine")
   }
+ $scripts = "c:\scripts"
+if (!$Env:Path.contains($scripts)){ 
+  [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $scripts+";", "Machine")
+  }  	
 
-$winsdk = "c:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\"
+#TODO: if 64 then else
+$winsdk = "c:\Program Files (x86)\Windows Kits\8.0\bin\x64\"
 if (!$Env:Path.contains($winsdk)){ 
   [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $winsdk, "Machine")
   }  
 $Env:Path
-$extensions =".fsx;"
-$Env:PATHEXT = $Env:PATHEXT +";"+$extensions
+$fsx =".fsx;"	
+if (!$Env:PATHEXT.contains($fsx)){ 
+        $Env:PATHEXT = $Env:PATHEXT +";"+$fsx
+}  
+
+$truecrypt = "c:\Program Files\trueCrypt\"
+if (!$Env:Path.contains($truecrypt)){ 
+  [Environment]::SetEnvironmentVariable("PATH",$Env:Path + $truecrypt, "Machine")
+  }  
+  
+
---
+++ run-work.bat
@@ -0,0 +1,3 @@
+:: start up script when Dzmitry Lahoda is in office
+truecrypt /volume "i:\!private.tc" /letter "X" /auto
+truecrypt /volume "i:\!epam.tc" /letter "V" /auto
