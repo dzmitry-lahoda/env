5936290fdea5df4699d644efcbf7cc3137ad2e95
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-11-06 16:06:55 +0300

    up dump process script

--- dump32.bat
+++ dump32.bat
@@ -1,2 +1,4 @@
-:: makes dumps of 32 bit processes
-"c:\Program Files (x86)\Debugging Tools for Windows (x86)\adplus" -hang -pn EXCEL.EXE -quiet
+:: Makes dumps of 32 bit processes into script directory while process in hang or crashing state
+:: Example:
+::   dump32 Excel.exe 
+"c:\SkyDrive\exe\Debugging Tools for Windows (x86)\adplus" -hang -pn %1 -quiet -o %~dp0
