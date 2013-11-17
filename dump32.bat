a74e2a6b57ef8fac2148731cc143a2807ab78eee
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-11-17 13:29:39 +0300

    up dump util

--- dump32.bat
+++ dump32.bat
@@ -1,4 +1,5 @@
 :: Makes dumps of 32 bit processes into script directory while process in hang or crashing state
 :: Example:
 ::   dump32 Excel.exe 
-"c:\SkyDrive\exe\Debugging Tools for Windows (x86)\adplus" -hang -pn %1 -quiet -o %~dp0
+SET debugger="c:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86\adplus"
+%debugger% -hang -pn %1 -quiet -o %~dp0
