589ea38ff2927fdd42b6bcd2c15e20fb6888e473
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-04-22 23:12:38 +0300

    made thinkgs safer

--- getpsw.bat
+++ getpsw.bat
@@ -4,4 +4,7 @@
 ::   getpsw
 ::   echo %password%
 @echo off
-powershell -Command $pword = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp.txt & set /p password=<.tmp.txt & del .tmp.txt
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
