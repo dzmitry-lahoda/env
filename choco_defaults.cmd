86b098671b22be93f6b1170532bfc818f732907b
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-10-26 13:35:17 +0300

    added favorite choco apps

---
+++ choco_defaults.cmd
@@ -0,0 +1,6 @@
+:: installs basic desktop software to be used
+choco install jpegview
+choco install doublecmd
+choco install notepadplusplus
+choco install sumatrapdf
+choco install truecrypt
---
+++ rotate.py
@@ -0,0 +1,5 @@
+#PY  <- Needed to identify #
+#--automatically built--
+
+adm = Avidemux()
+adm.addVideoFilter("rotate", "angle=270")
