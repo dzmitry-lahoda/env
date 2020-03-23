52ba3ece0c5b49e5fc1386a586289bc2790675d7
dzmitry.lahoda <dzmitry@lahoda.pro>
2020-03-23 04:25:32 -0700

    video convesion

--- ffmpeg-to-mp4.ps1
+++ ffmpeg-to-mp4.ps1
@@ -4,4 +4,8 @@ param (
     [string]$filename
 )
 
-ffmpeg -i $filename -strict -2 -c copy "${$filename}.mp4" 
+ffmpeg -i $filename -strict -2 -c copy "$filename.mp4" 
+#[mp4 @ 00000161a5a64980] Could not find tag for codec flv1 in stream #0, codec not currently supported in container
+#Could not write header for output file #0 (incorrect codec parameters ?): Invalid argument
+# TODO: in case of error 
+# ffmpeg -i $filename -vcodec libx264 -acodec copy "$filename.mp4"
