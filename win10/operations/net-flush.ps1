5f6bba4b8ebd2508e3143a838c907171f5ca047e
dzmitry-lahoda <dzmitry@lahoda.pro>
2020-09-02 14:18:03 +0300

    cleanup

--- win10/operations/net-flush.ps1
+++ win10/operations/net-flush.ps1
@@ -1,23 +1,17 @@
-:: Cleans network stack caches. Sometimes help with networking problems
-
-:: TODO: prefix logs with script name and put logs in current dir
-
+# Cleans network stack caches. Sometimes help with networking problems
+# TODO: prefix logs with script name and put logs in current dir
 ipconfig /flushdns
-
 netsh interface ip delete arpcache
-
 ipconfig /release
-
 ipconfig /renew
-
-:: reset TCP/IP stack
+# reset TCP/IP stack
 
 netsh int ip reset c:\resetlog.txt   
 
-:: reset winsock
+# reset winsock
 
 netsh winsock reset c:\winsock.txt
 
-:: reset IPv4 TCP/IP stack
+# reset IPv4 TCP/IP stack
 
 netsh int ipv4 reset C:\reset.log
